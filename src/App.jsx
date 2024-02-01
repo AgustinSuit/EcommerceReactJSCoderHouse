56a971e0b264309d5199d767977e69d172d691dc
Agustin Bracamonte <AgustinPorato@hotmail.com>
2024-02-01 19:59:42 -0300

    Cambio en URL

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
                 <CartProvider>
                     <NavBar />
                     <Routes>
-                        <Route path="/" element={<ItemListContainer greeting={"Todos nuestros productos"} />} />
+                        <Route path="/EcommerceReactJSCoderHouse" element={<ItemListContainer greeting={"Todos nuestros productos"} />} />
                         <Route path="/category/:categoryId" element={<ItemListContainer greeting={"Productos por Categoría"} />} />
                         <Route path="/item/:itemId" element={<ItemDetailContainer />} />
                         <Route path="/cart" element={<Cart />} />
